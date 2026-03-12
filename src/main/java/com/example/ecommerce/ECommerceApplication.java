973a09371a6ef8fa9775fe656f51ba06720d5d0d
Kayima909 <davidkaymer@gmail.com>
2026-03-12 22:13:01 +0300

    Added @ComponentScan to springboot class

--- src/main/java/com/example/ecommerce/ECommerceApplication.java
+++ src/main/java/com/example/ecommerce/ECommerceApplication.java
@@ -2,7 +2,9 @@ package com.example.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.example.ecommerce")
 @SpringBootApplication
 public class ECommerceApplication {
 
